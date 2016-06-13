fcb1f846da806a4509d254f2295ea5a4bb12a961
Mandarin Drummond <mandarind@gmail.com>
2016-06-13 07:45:47 -0600

    add scroll with offset prop

---
+++ README-shoflo.md
@@ -0,0 +1,8 @@
+To build...
+
+1. run buildNPMInternals shell script (don't use npm run, just run shell script directly)
+2. copy 'internal' directory to 'internal.new'
+3. check out npm-pkg-0.6.0 branch 
+4. move 'internal.new' to 'internal'
+5. Remove .babelrc file from npm-pkg-0.6.0 branch
+6. If necessary, install react-dom so it will build.. (npm install react-dom)
--- src/FixedDataTableNew.react.js
+++ src/FixedDataTableNew.react.js
@@ -357,6 +357,12 @@ var FixedDataTable = React.createClass({
   },
 
   componentWillReceiveProps(/*object*/ nextProps) {
+
+    var scrollToRowWithOffset = nextProps.scrollToRowWithOffset;
+    if (scrollToRowWithOffset !== undefined && scrollToRowWithOffset !== null) {
+      this._rowToScrollToWithOffset = scrollToRowWithOffset;
+    }
+
     var scrollToRow = nextProps.scrollToRow;
     if (scrollToRow !== undefined && scrollToRow !== null) {
       this._rowToScrollTo = scrollToRow;
@@ -760,6 +766,15 @@ var FixedDataTable = React.createClass({
       firstRowIndex = scrollState.index;
       firstRowOffset = scrollState.offset;
       scrollY = scrollState.position;
+    }
+
+    if (this._rowToScrollToWithOffset !== undefined) {
+      scrollState =
+        this._scrollHelper.scrollToRow(this._rowToScrollToWithOffset, 300);
+      firstRowIndex = scrollState.index;
+      firstRowOffset = scrollState.offset;
+      scrollY = scrollState.position;
+      delete this._rowToScrollToWithOffset;
     }
 
     if (this._rowToScrollTo !== undefined) {
